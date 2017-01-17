b64af005047ea1827dccc3bf6da0557a3cd6e2da
Krishna <krishna.godase@interactive.in>
2017-01-17 19:47:20 +0530

    abc

---
+++ src/App_modules/ExcelTest.java
@@ -0,0 +1,95 @@
+package App_modules;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+//import org.junit.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import App_modules.MD04_Draw_Flow;
+
+
+
+//import org.testng.annotations.Test;
+
+public class ExcelTest {
+
+	XSSFWorkbook wb;
+	XSSFSheet sh1;
+	int rowCount;
+	
+	
+	Automation_Framework.Locators ls = new Automation_Framework.Locators();
+	App_modules.MD04_Draw_Flow df; 
+
+	@BeforeTest
+	public void exceldata() {
+		try {
+
+			// Specify the file path which you want to create or write
+			File src = new File(ls.excelFileLoc);
+
+			// Load the file
+			FileInputStream fis = new FileInputStream(src);
+
+			// load the workbook
+			// @SuppressWarnings("resource")
+			@SuppressWarnings("resource")
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+			// get the sheet which you want to modify or create
+			sh1 = wb.getSheetAt(0);
+
+			System.out.println(sh1.getFirstRowNum());
+
+			for (int i = 1; 1 <= sh1.getLastRowNum() - 1; i++) 
+			{
+
+				// getRow specify which row we want to read and getCell which
+				// column
+
+				XSSFCell mobile = sh1.getRow(i).getCell(0);
+				XSSFCell firstname = sh1.getRow(i).getCell(1);
+				XSSFCell lastname = sh1.getRow(i).getCell(2);
+				String emailid = sh1.getRow(i).getCell(3).getStringCellValue();
+				XSSFCell otp = sh1.getRow(i).getCell(4);
+				XSSFCell postalcode = sh1.getRow(i).getCell(5);
+
+				
+				MD04_Draw_Flow.data(mobile, firstname, lastname,emailid, otp, postalcode);
+				
+				df =new MD04_Draw_Flow();
+				df.setUp();
+				df.Signin_success();
+			    rowCount();
+			}
+
+		} catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+			return;
+		}
+		
+		
+	}
+
+	@Test
+	public int rowCount() 
+	{
+		int rowNo = 0;
+		try 
+		{
+			rowNo = sh1.getLastRowNum();
+		}
+		catch (Exception e) 
+		{
+			System.out.println(e.getMessage());
+		}
+		return rowNo;
+	}
+
+}
